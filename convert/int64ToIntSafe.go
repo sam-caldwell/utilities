e9a6f1ceece5d09e8911a158343fbbb50b64e13b
sam caldwell <mail@samcaldwell.net>
2023-06-15 15:46:02 -0500

    add convert.int64toIntSafe()

---
+++ convert/int64ToIntSafe.go
@@ -0,0 +1,15 @@
+package convert
+
+import "fmt"
+
+// int64ToIntSafe - safely convert int64 to integer
+func int64ToIntSafe(num int64) (int, error) {
+	const maxInt = int(^uint(0) >> 1)
+	const minInt = -maxInt - 1
+
+	if num > int64(maxInt) || num < int64(minInt) {
+		return 0, fmt.Errorf("integer overflow")
+	}
+
+	return int(num), nil
+}
